577c4fc35ae5474342c045eea0ed5a01ea6e51ee
ShubhamLaad <shubhamlaad666@gmail.com>
2020-08-31 00:16:12 +0530

    Fix fooer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,13 +16,13 @@ export default function Footer(props) {
     <footer>
       <Container className="d-flex">
         <p>© 2016 Sparsh NGO</p>
-        <a className="developer" rel="noopener noreferrer" href="https://www.linkedin.com/in/shubhamlaad" target="_blank">Developed By: Shubham Laad</a>
+        <a className="developer" rel="noopener noreferrer" href="https://www.linkedin.com/in/shubhamlaad" target="_blank">Developed By<br /> Shubham Laad</a>
         <ul className="d-flex social-list">
           {SOCIAL_ICONS.map(social => <li key={social.title}>
             <a target="_blank" rel="noopener noreferrer" title={social.title} href={social.href}>{social.icon}</a>
           </li>)}
         </ul>
       </Container>
-    </footer>
+    </footer >
   );
 }
