c7b9341a37a25b95844d664d7d582c8c48a5700d
ShubhamLaad <shubhamlaad666@gmail.com>
2020-09-03 00:26:09 +0530

    Developer UI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ export default function Footer(props) {
     <footer>
       <Container className="d-flex">
         <p>© 2016 Sparsh NGO</p>
-        <a className="developer" rel="noopener noreferrer" href="https://www.linkedin.com/in/shubhamlaad" target="_blank">Developed By<br /> Shubham Laad</a>
+        <a className="developer" rel="noopener noreferrer" href="https://www.linkedin.com/in/shubhamlaad" target="_blank">Developed By<div className="shubham"> Shubham Laad</div></a>
         <ul className="d-flex social-list">
           {SOCIAL_ICONS.map(social => <li key={social.title}>
             <a target="_blank" rel="noopener noreferrer" title={social.title} href={social.href}>{social.icon}</a>
